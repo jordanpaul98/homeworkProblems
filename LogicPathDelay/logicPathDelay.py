7b4fe12e6d7e9c581e570b0fc7c80a7cf2f88fd8
jordanpaul98 <147276895+jordanpaul98@users.noreply.github.com>
2023-11-08 03:29:07 -0600

    Update logicPathDelay.py

--- LogicPathDelay/logicPathDelay.py
+++ LogicPathDelay/logicPathDelay.py
@@ -4,20 +4,20 @@ Jordan Paul:
 Logic Path Delay: Created for homework 5 problem 2 to display logic expression
 
 create and get the path delay from input H
-Note: this version onlys works for balanced non branching circuits as B is expected to be 1
+Note: this version only works for balanced non branching circuits as B is expected to be 1
 
 Nand, Nor and inverters.
 
-create a logic cirucit by Logic() with any number of series input gates
+create a logic circuit by Logic() with any number of series input gates
 
 Ex. 4 input AND gate, is a 4 Input NAND with a Inverter on its output, the logic setup is as follows
-logic = Logic(Nand(4), Inverter()) 
+logic = Logic(Nand(4), Inverter())
 
 calling logic.delay(1) or any number H will return total path delay D
-logic.pathEffort(H) will return the path effort usally noted as F multiplied by H
-logic.pathDelay() will return the path delay usally noted as P is the sum of the gates parasitic delay
+logic.pathEffort(H) will return the path effort usually noted as F multiplied by H
+logic.pathDelay() will return the path delay usually noted as P is the sum of the gates parasitic delay
 logic.stages() will return number of stages, Noted as N or can use len(Logic)
-print(Logic) will print out Logic Path expression: 
+print(Logic) will print out Logic Path expression:
   Ex.
   Name (if given):  Stages- (N)   Delay = N * (H * pathEffort()) ^ (1 / N) + pathDelay()
 
@@ -79,7 +79,8 @@ def Problem2PathEffort():
             return len(self) * self.pathEffort(H) ** (1. / len(self)) + self.pathDelay()
 
         def __str__(self):
-            return f"{self.name}{':  ' if self.name else ''}Stages- {len(self)}   Delay = {len(self)} * (H * {self.pathEffort():.3f}) ^ (1 / {len(self)}) + {self.pathDelay()}"
+            return f"{self.name}{':  ' if self.name else ''}Stages- {len(self)}" \
+                   f"   Delay = {len(self)} * (H * {self.pathEffort():.3f}) ^ (1 / {len(self)}) + {self.pathDelay()}"
 
     a = Logic(Nand(6), Inverter(), name='a')
     b = Logic(Nand(3), Nor(2), name='b')
